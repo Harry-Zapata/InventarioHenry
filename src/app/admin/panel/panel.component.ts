5ee68489637a7528171b0cd3c390a1f40ab73636
Harry-Zapata <harryzapatasoto@gmail.com>
2023-09-03 19:13:24 -0500

    responsive

--- src/app/admin/panel/panel.component.ts
+++ src/app/admin/panel/panel.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./panel.component.css']
 })
 export class PanelComponent {
+menu(){
+  let anchoNavegador = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  let body = document.getElementsByTagName('body')[0];
+  let leftPanel = document.getElementById('left-panel');
+  if(anchoNavegador<1008){
+    if (leftPanel!=null) {
+      leftPanel.style.display=="" || leftPanel.style.display=="none" ? leftPanel.style.display="block": leftPanel.style.display="none";
+    }
+  }else{
+    body.classList.value=="" ? body.classList.add("open"): body.classList.remove("open");   
+  }
+}
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    let anchoNavegador = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    let body = document.getElementsByTagName('body')[0];
+    if (anchoNavegador<1008) {
+      body.classList.add("small-device");
+    }
+  }
   title = 'libreria';
 }
